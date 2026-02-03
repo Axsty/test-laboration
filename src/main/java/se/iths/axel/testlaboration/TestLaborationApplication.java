1e1a7863c14a799ce20323f3d4b0b36ed18b1025
Axsty <axelstaaffriborg@gmail.com>
2026-02-03 13:55:22 +0100

    Initial commit

---
+++ src/main/java/se/iths/axel/testlaboration/TestLaborationApplication.java
@@ -0,0 +1,13 @@
+package se.iths.axel.testlaboration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLaborationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLaborationApplication.class, args);
+	}
+
+}
