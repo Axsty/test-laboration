23e22b86008f685ce28f23db54d7a3f19d7ffca5
Axsty <axelstaaffriborg@gmail.com>
2026-02-04 21:50:58 +0100

    Uppdaterat workflow, playwrighttest

--- src/test/java/se/iths/axel/testlaboration/playwright/PlaywrightTest.java
+++ src/test/java/se/iths/axel/testlaboration/playwright/PlaywrightTest.java
@@ -1,6 +1,7 @@
 package se.iths.axel.testlaboration.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,9 @@ public class PlaywrightTest {
     @Test
     public void playwrightTitleTest() {
         try (Playwright playwright = Playwright.create();
-             Browser browser = playwright.firefox().launch()) {
+             Browser browser = playwright.firefox().launch(
+                     new BrowserType.LaunchOptions().setHeadless(true)
+             )) {
 
             Page page = browser.newPage();
             page.navigate("http://localhost:8080/balance");
@@ -24,7 +27,9 @@ public class PlaywrightTest {
     @Test
     public void playwrightBalanceTest() {
         try (Playwright playwright = Playwright.create();
-             Browser browser = playwright.firefox().launch()) {
+             Browser browser = playwright.firefox().launch(
+                     new BrowserType.LaunchOptions().setHeadless(true)
+             )) {
 
             Page page = browser.newPage();
             page.navigate("http://localhost:8080/balance");
