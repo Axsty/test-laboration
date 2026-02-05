b9f18ab76462860de7d18b6328f72d7966c445cf
Axsty <axelstaaffriborg@gmail.com>
2026-02-05 10:38:54 +0100

    Html och thymeleaf.

--- src/main/java/se/iths/axel/testlaboration/controller/HomeController.java
+++ src/main/java/se/iths/axel/testlaboration/controller/HomeController.java
@@ -3,6 +3,7 @@ package se.iths.axel.testlaboration.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.axel.testlaboration.service.ATMService;
 
@@ -20,5 +21,17 @@ public class HomeController {
     public String getBalance(Model model) {
         model.addAttribute("balance", atmService.getBalance());
         return "home";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(int amount) {
+        atmService.withdraw(amount);
+        return "redirect:/balance";
+    }
+
+    @PostMapping("/deposit")
+    public String deposit(int amount) {
+        atmService.deposit(amount);
+        return "redirect:/balance";
     }
 }
